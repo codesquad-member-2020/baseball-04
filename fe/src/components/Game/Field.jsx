70e0c831da8f2f8b26bacd23805141b8dd8ad1d6
랄랄랄 <hyewon3938@naver.com>
2020-05-09 01:09:13 +0900

    [FE] feat : 진행현황판 애니메이션 구현을 위한 UI 배치
    - div에 선수가 움직이는 gif 를 담아 야구경기장 다이아몬드 모양으로 각 방향 총 4개를 배치함.
    - issue #11

--- fe/src/components/Game/Field.jsx
+++ fe/src/components/Game/Field.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled, { keyframes } from "styled-components";
-import filedImg from '../../images/field.png';
+import filedImg from '../../images/diamond2.png';
+// import filedImg from '../../images/field2.png';
+// import filedImg from '../../images/diamond.jpg';
+
 
 
 const Field = () => {
@@ -44,8 +47,67 @@ const Field = () => {
 
     `;
     const FieldImg = styled.img`
-        height : 550px;
+        width : 600px;
+        height : 530px;
         margin-top : 10px;
+        border-radius : 10px;
+    `;
+
+    const Route1 = styled.div`
+      /* border-bottom : solid 3px red; */
+      position: absolute;
+      width: 320px;
+      top: 570px;
+      left: 500px;
+      transform: rotate(-47deg);
+    `;
+    const Route2 = styled.div`
+      /* border-bottom : solid 3px red; */
+      position: absolute;
+      width: 280px;
+      top: 375px;
+      left: 570px;
+      transform: rotate(39deg);
+    `;
+     const Route3 = styled.div`
+       /* border-bottom : solid 3px red; */
+       position: absolute;
+       width: 280px;
+       top: 370px;
+       left: 325px;
+       transform: rotate(-38deg);
+     `;
+
+     const Route4 = styled.div`
+       /* border-bottom : solid 3px red; */
+       position: absolute;
+       width: 320px;
+       top: 565px;
+       left: 345px;
+       transform: rotate(48deg);
+     `;
+    const AniImg = styled.img`
+      height: 70px;
+      margin-left: 150px;
+      /* transform:rotate(0deg);
+        -moz-transform: scaleX(-1); 
+        -o-transform: scaleX(-1); 
+        -webkit-transform: scaleX(-1); 
+        transform: scaleX(-1);   
+        filter: FlipH;
+        -ms-filter: "FlipH"; */
+    `;
+
+    const AniImg2 = styled.img`
+      height: 70px;
+      margin-left: -30px;
+      transform: rotate(0deg);
+      -moz-transform: scaleX(-1);
+      -o-transform: scaleX(-1);
+      -webkit-transform: scaleX(-1);
+      transform: scaleX(-1);
+      filter: FlipH;
+      -ms-filter: "FlipH";
     `;
 
     return (
@@ -55,6 +117,18 @@ const Field = () => {
                 <SBO>B<Count style={{color : 'green'}}>●●●</Count></SBO>
                 <SBO>O<Count style={{color : 'red'}}>●●</Count></SBO>
             </SBOWrap>
+            <Route1>
+                <AniImg src="https://media2.giphy.com/media/YrCNxwsXSVLlw0TY1R/giphy.gif?cid=ecf05e47699788e3a0754d3861099fa54128c0c53f4695a0&rid=giphy.gif" />
+            </Route1>
+            <Route2>
+                <AniImg2 src="https://media2.giphy.com/media/YrCNxwsXSVLlw0TY1R/giphy.gif?cid=ecf05e47699788e3a0754d3861099fa54128c0c53f4695a0&rid=giphy.gif" />
+            </Route2>
+            <Route3>
+                <AniImg2 style={{marginLeft:"50px"}} src="https://media2.giphy.com/media/YrCNxwsXSVLlw0TY1R/giphy.gif?cid=ecf05e47699788e3a0754d3861099fa54128c0c53f4695a0&rid=giphy.gif" />
+            </Route3>
+            <Route4>
+                <AniImg  src="https://media2.giphy.com/media/YrCNxwsXSVLlw0TY1R/giphy.gif?cid=ecf05e47699788e3a0754d3861099fa54128c0c53f4695a0&rid=giphy.gif" />
+            </Route4>
             <FieldImg src={filedImg} />
             <GameInfo>* 2회초 수비</GameInfo>
         </Wrap>
