17940cdaa42f2b8396e090451ee9c440e5107cee
랄랄랄 <hyewon3938@naver.com>
2020-05-08 22:14:03 +0900

    [FE] feat : 선수정보 UI 구현
    - issue #11

--- fe/src/components/Game/PlayerInfo.jsx
+++ fe/src/components/Game/PlayerInfo.jsx
@@ -5,14 +5,69 @@ import styled, { keyframes } from "styled-components"
 const PlayerInfo = () => {
 
     const Wrap = styled.div`
-        width : 400px;
+        width : 380px;
         height : 200px;
-        border : solid 2px white;
         box-sizing : border-box;
+        background: rgba(255,255,255, 0.2);
+        margin : 10px;
+        padding : 20px;
+        border-radius : 10px;
+    `;
+
+    const PlayerWrap = styled.div`
+        margin-bottom : 15px;
+    `;
+
+    const Position = styled.div`
+        font-family : 'NeoDunggeunmo';
+        font-size : 20px;
+        margin-left : 5px;
+        color : white;
+    `;
+
+    const InfoWrap = styled.div`
+        display : flex;
+        height : 45px;
+        margin-top :10px;
+        /* padding : 5px 5px 5px 10px; */
+        box-sizing : border-box;
+
+    `;
+
+    const Player = styled.div`
+        font-family : 'NeoDunggeunmo';
+        height : 100%;
+        line-height :45px;
+        color : white;
+        font-size : 25px;
+        padding : 0px 5px 0px 5px;
+        background : #A33756;
+        box-sizing : border-box;
+
+    `;
+
+    const Info = styled.div`
+        font-family : 'NeoDunggeunmo';
+        height : 100%;
+        line-height : 45px;
+        color : #28d685;
+        font-size : 25px;
+        margin-left : 10px;
+        box-sizing : border-box;
+
     `;
 
     return (
-      <Wrap></Wrap>
+      <Wrap>
+          <PlayerWrap>
+              <Position>[ 투수 ]</Position>
+              <InfoWrap><Player>데이비드</Player><Info>#39</Info></InfoWrap>
+          </PlayerWrap>
+          <PlayerWrap>
+              <Position>[ 타자 ]</Position>
+              <InfoWrap><Player>하밀</Player><Info>1타석 0안타</Info> </InfoWrap>
+          </PlayerWrap>
+      </Wrap>
     );
 };
 
