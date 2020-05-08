c27ad219970a764907e4b11a181c359e43c676dc
랄랄랄 <hyewon3938@naver.com>
2020-05-08 19:29:48 +0900

    [FE] feat : 진행현황판 UI 구현
    - 야구장 field 이미지는 애니메이션 구현하기 불편해보여서 추후에 수정 예정
    - issue #11

--- fe/src/components/Game/Field.jsx
+++ fe/src/components/Game/Field.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled, { keyframes } from "styled-components"
+import styled, { keyframes } from "styled-components";
+import filedImg from '../../images/field.png';
 
 
 const Field = () => {
@@ -7,13 +8,56 @@ const Field = () => {
     const Wrap = styled.div`
         width : 1100px;
         height : 600px;
-        border : solid 2px white;
         box-sizing : border-box;
+        display : flex;
+        justify-content:space-between;
+        padding : 10px;
+    `;
+
+    const SBOWrap = styled.div`
+        width : 200px;
+        padding : 10px;
+    `;
+
+    const SBO = styled.div`
+        font-family : 'NeoDunggeunmo';
+        color : white;
+        font-size : 35px;
+        margin : 5px;
+        display :flex;
+    `;
+
+    const Count = styled.div`
+        font-family : 'NeoDunggeunmo';
+        color : white;
+        font-size : 35px;
+        margin-left :15px;
     `;
 
 
+    const GameInfo = styled.div`
+        font-family : 'NeoDunggeunmo';
+        font-size : 30px;
+        color : white;
+        padding : 15px;
+        margin-right : 10px;
+
+    `;
+    const FieldImg = styled.img`
+        height : 550px;
+        margin-top : 10px;
+    `;
+
     return (
-        <Wrap></Wrap>
+        <Wrap>
+            <SBOWrap>
+                <SBO>S <Count style={{color : 'yellow'}}>●●</Count></SBO>
+                <SBO>B<Count style={{color : 'green'}}>●●●</Count></SBO>
+                <SBO>O<Count style={{color : 'red'}}>●●</Count></SBO>
+            </SBOWrap>
+            <FieldImg src={filedImg} />
+            <GameInfo>2회초 수비</GameInfo>
+        </Wrap>
     );
 };
 
