df7702149b6100aa6b98639de6e4886d219c226b
dev-Lena <keunnalee@gmail.com>
2020-05-12 03:00:45 +0900

    [iOS] feat: UIBezierPath로 둥근 사각형을 그리는 view 추가
    - IBDesignable, IBInspectable 사용
    - Issue #31

---
+++ iOS/BaseBallGame/BaseBallGame/View/RoundRectView.swift
@@ -0,0 +1,34 @@
+//
+//  roundView.swift
+//  BaseBallGame
+//
+//  Created by Keunna Lee on 2020/05/11.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundRectView: UIView {
+
+    @IBInspectable var colorToFill: UIColor  = .clear
+    @IBInspectable var borderColor: UIColor  = .darkGray
+    @IBInspectable var borderWidth: CGFloat  = 5
+    @IBInspectable var cornerRadiusAngle: CGFloat  = 15.0
+    @IBInspectable var allroundingCorners: Bool = true
+    
+
+    override func draw(_ rect: CGRect) {
+        drawRoundRect(rect: rect)
+    }
+    
+    private func drawRoundRect(rect: CGRect) {
+        
+        let path = UIBezierPath(roundedRect: self.bounds, cornerRadius: cornerRadiusAngle)
+        path.lineWidth = borderWidth
+        colorToFill.setFill()
+        borderColor.setStroke()
+        path.fill()
+        path.stroke()
+    }
+}
