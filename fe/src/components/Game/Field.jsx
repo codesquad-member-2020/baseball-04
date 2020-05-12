76f223f40d535fbad445558029a0797a0a61b704
랄랄랄 <hyewon3938@naver.com>
2020-05-12 15:14:02 +0900

    [FE] refactor : pitch 버튼 다른 컴포넌트로 분리
    - Pitch 버튼의 스타일을 useState로 애니메이션과 같은 컴포넌트에서 관리를 하니까 애니메이션도 pitch버튼 스타일의 상태가 변경될 때마다 다시 렌더링 되는 문제 때문에 컴포넌트를 분리했다.

--- fe/src/components/Game/Field.jsx
+++ fe/src/components/Game/Field.jsx
@@ -1,104 +1,62 @@
 import React, { useState, useEffect } from "react";
 import styled, { css, keyframes } from "styled-components";
-import filedImg from '../../images/diamond2.png';
+import filedImg from "../../images/diamond2.png";
+import PitchBtn from "./PitchBtn";
+
 
 const Field = () => {
+  const [batterPosition, setBatterPosition] = useState(0);
 
-    const [batterPosition, setBatterPosition] = useState(0);
-    const [btnStyle, setBtnStyle] = useState({display : "none"});
+  const Wrap = styled.div`
+    width: 1000px;
+    height: 600px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    position: relative;
+    overflow: hidden;
+  `;
 
-    const Wrap = styled.div`
-        width : 1000px;
-        height : 600px;
-        box-sizing : border-box;
-        display : flex;
-        justify-content:space-between;
-        padding : 10px;
-        position : relative;
-        overflow : hidden;
-    `;
+  const SBOWrap = styled.div`
+    width: 200px;
+    padding: 10px;
+  `;
 
-    const SBOWrap = styled.div`
-        width : 200px;
-        padding : 10px;
-    `;
+  const SBO = styled.div`
+    font-family: "NeoDunggeunmo";
+    color: white;
+    font-size: 35px;
+    margin: 5px;
+    display: flex;
+  `;
 
-    const SBO = styled.div`
-        font-family : 'NeoDunggeunmo';
-        color : white;
-        font-size : 35px;
-        margin : 5px;
-        display :flex;
-    `;
+  const Count = styled.div`
+    font-family: "NeoDunggeunmo";
+    color: white;
+    font-size: 35px;
+    margin-left: 15px;
+  `;
 
-    const Count = styled.div`
-        font-family : 'NeoDunggeunmo';
-        color : white;
-        font-size : 35px;
-        margin-left :15px;
-    `;
+  const GameInfo = styled.div`
+    font-family: "NeoDunggeunmo";
+    font-size: 30px;
+    color: white;
+    padding: 25px;
+    width: 200px;
+    margin: 0px 10px;
+  `;
+  const FieldImg = styled.img`
+    width: 600px;
+    height: 530px;
+    margin-top: 10px;
+    border-radius: 10px;
+  `;
 
+  
 
-    const GameInfo = styled.div`
-        font-family : 'NeoDunggeunmo';
-        font-size : 30px;
-        color : white;
-        padding : 25px;
-        width : 200px;
-        margin : 0px 10px;
-
-    `;
-    const FieldImg = styled.img`
-        width : 600px;
-        height : 530px;
-        margin-top : 10px;
-        border-radius : 10px;
-    `;
-
-    const PitchBtn = styled.button`
-        position : absolute;
-        font-family : 'NeoDunggeunmo';
-        top : 230px;
-        left : 427px;
-        font-size : 30px;
-        border-radius : 10px;
-        outline : 0;
-        background : #BDBDBD;
-    `;
-
-    const PitchBtnWrap = styled.div`
-        position : absolute;
-        top : 200px;
-        left : 405px;
-        width : 150px;
-        justify-content : center;
-        margin : 2px;
-        display : none ; 
-        
-    `;
-    const BallBtn = styled.button`
-        font-family : 'NeoDunggeunmo';
-        font-size : 20px;
-        background : #E26D07;
-        color : white;
-        border-radius : 10px;
-        outline :0;
-        border : 0;
-        margin-right :5px;
-    `;
-
-    const StrikeBtn = styled.button`
-      font-family: "NeoDunggeunmo";
-      font-size: 20px;
-      background: blue;
-      color: white;
-      border-radius: 10px;
-      outline: 0;
-      border: 0;
-    `;
-
-    //초기 0, 이동 270
-    const move1 = keyframes`
+  //초기 0, 이동 270
+  const move1 = keyframes`
     0%{
      margin-left : 0px;
     }
@@ -106,8 +64,8 @@ const Field = () => {
      margin-left : 270px;
     }
     `;
-    // 초기 0, 이동 210 -> -25
-    const move23 = keyframes`
+  // 초기 0, 이동 210 -> -25
+  const move23 = keyframes`
     0% {
      margin-left : 210px;
     }
@@ -115,7 +73,7 @@ const Field = () => {
      margin-left : -25px;
      }
     `;
-    const move4 = keyframes`
+  const move4 = keyframes`
     0%{
      margin-left : 0px;
     }
@@ -127,134 +85,179 @@ const Field = () => {
    }
     
     `;
-    const Route1 = styled.div`
-      /* border-bottom : solid 3px red; */
-      position: absolute;
-      width: 320px;
-      top: 305px;
-      left: 385px;
-      transform: rotate(-50deg);
-    `;
-    const Route2 = styled.div`
-      /* border-bottom : solid 3px red; */
-      position: absolute;
-      width: 280px;
-      top: 110px;
-      left: 450px;
-      transform: rotate(42deg);
-    `;
-     const Route3 = styled.div`
-       /* border-bottom : solid 3px red; */
-       position: absolute;
-       width: 280px;
-       top: 105px;
-       left: 235px;
-       transform: rotate(-40deg);
-     `;
+  const Route1 = styled.div`
+    position: absolute;
+    width: 320px;
+    top: 305px;
+    left: 385px;
+    transform: rotate(-50deg);
+  `;
+  const Route2 = styled.div`
+    position: absolute;
+    width: 280px;
+    top: 110px;
+    left: 450px;
+    transform: rotate(42deg);
+  `;
+  const Route3 = styled.div`
+    position: absolute;
+    width: 280px;
+    top: 105px;
+    left: 235px;
+    transform: rotate(-40deg);
+  `;
 
-     const Route4 = styled.div`
-       /* border-bottom : solid 3px red; */
-       position: absolute;
-       width: 330px;
-       top: 295px;
-       left: 230px;
-       transform: rotate(50deg);
-     `;
-    const AniImg1 = styled.img`
-      height: 70px;
-      margin-left : 0px;
-     ${props => {
-         if(props.active){
-             return css`animation : ${move1} 2s forwards`;
-         }
-     }}
-    `;
+  const Route4 = styled.div`
+    position: absolute;
+    width: 330px;
+    top: 295px;
+    left: 230px;
+    transform: rotate(50deg);
+  `;
+  const AniImg1 = styled.img`
+    height: 70px;
+    margin-left: 0px;
+    ${(props) => {
+      if (props.active) {
+        return css`
+          animation: ${move1} 2s forwards;
+        `;
+      }
+    }}
+  `;
 
-    const AniImg2 = styled.img`
-      height: 70px;
-      margin-left : 210px;
-      transform: rotate(0deg);
-      -moz-transform: scaleX(-1);
-      -o-transform: scaleX(-1);
-      -webkit-transform: scaleX(-1);
-      transform: scaleX(-1);
-      filter: FlipH;
-      -ms-filter: "FlipH";
-      ${props => {
-         if(props.active){
-             return css`animation : ${move23} 2s forwards`;
-         }
-     }}
-    `;
+  const AniImg2 = styled.img`
+    height: 70px;
+    margin-left: 210px;
+    transform: rotate(0deg);
+    -moz-transform: scaleX(-1);
+    -o-transform: scaleX(-1);
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+    filter: FlipH;
+    -ms-filter: "FlipH";
+    ${(props) => {
+      if (props.active) {
+        return css`
+          animation: ${move23} 2s forwards;
+        `;
+      }
+    }}
+  `;
 
-    const AniImg3 = styled.img`
-      height: 70px;
-      margin-left : 210px;
-      transform: rotate(0deg);
-      -moz-transform: scaleX(-1);
-      -o-transform: scaleX(-1);
-      -webkit-transform: scaleX(-1);
-      transform: scaleX(-1);
-      filter: FlipH;
-      -ms-filter: "FlipH";
-      ${props => {
-         if(props.active){
-             return css`animation : ${move23} 2s forwards`;
-         }
-     }}
-    `;
-    const AniImg4 = styled.img`
-      height: 70px;
-      margin-left: 0px;
-      ${props => {
-         if(props.active){
-             return css`animation : ${move4} 3.5s forwards`;
-         }
-     }}
-    `;
+  const AniImg3 = styled.img`
+    height: 70px;
+    margin-left: 210px;
+    transform: rotate(0deg);
+    -moz-transform: scaleX(-1);
+    -o-transform: scaleX(-1);
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+    filter: FlipH;
+    -ms-filter: "FlipH";
+    ${(props) => {
+      if (props.active) {
+        return css`
+          animation: ${move23} 2s forwards;
+        `;
+      }
+    }}
+  `;
+  const AniImg4 = styled.img`
+    height: 70px;
+    margin-left: 0px;
+    ${(props) => {
+      if (props.active) {
+        return css`
+          animation: ${move4} 4s forwards;
+        `;
+      }
+    }}
+  `;
 
+  const strikeBtnClickHandler = () => {
+    setBatterPosition(batterPosition + 1);
+  };
 
+  const runningAnimation = () => {
+    const img =
+      "https://media2.giphy.com/media/YrCNxwsXSVLlw0TY1R/giphy.gif?cid=ecf05e47699788e3a0754d3861099fa54128c0c53f4695a0&rid=giphy.gif";
 
+    if (batterPosition === 1) {
+      return (
+        <>
+          <Route1>
+            <AniImg1 active src={img} />
+          </Route1>
+        </>
+      );
+    }
+    if (batterPosition === 2) {
+      return (
+        <>
+          <Route1>
+            <AniImg1 active src={img} />
+          </Route1>
+          <Route2>
+            <AniImg2 active src={img} />
+          </Route2>
+        </>
+      );
+    }
+    if (batterPosition === 3) {
+      return (
+        <>
+          <Route1>
+            <AniImg1 active src={img} />
+          </Route1>
+          <Route2>
+            <AniImg2 active src={img} />
+          </Route2>
+          <Route3>
+            <AniImg3 active src={img} />
+          </Route3>
+        </>
+      );
+    }
+    if (4 <= batterPosition) {
+      return (
+        <>
+          <Route1>
+            <AniImg1 active src={img} />
+          </Route1>
+          <Route2>
+            <AniImg2 active src={img} />
+          </Route2>
+          <Route3>
+            <AniImg3 active src={img} />
+          </Route3>
+          <Route4>
+            <AniImg4 active src={img} />
+          </Route4>
+        </>
+      );
+    }
+  };
 
-
-    const pitchBtnClickHandler = ()=> {
-        setBtnStyle({display : "flex"});
-    };
-
-    const ballBtnClickHandler = ()=>{
-        setBtnStyle({display : "none"});
-
-    };
-
-    const strikeBtnClickHandler = ()=>{
-        setBtnStyle({display : "none"});
-        setBatterPosition(batterPosition + 1);
-    };
-
-    const runningAnimation = ()=> {
-
-        const img = "https://media2.giphy.com/media/YrCNxwsXSVLlw0TY1R/giphy.gif?cid=ecf05e47699788e3a0754d3861099fa54128c0c53f4695a0&rid=giphy.gif";
-        
-        if(batterPosition === 1){return <><Route1><AniImg1 active src={img}/></Route1></>}
-        if(batterPosition === 2){return <><Route1><AniImg1 active src={img}/></Route1><Route2><AniImg2 active src={img} /></Route2> </>}
-        if(batterPosition === 3){return <><Route1><AniImg1 active src={img} /></Route1><Route2><AniImg2 active src={img}/></Route2><Route3><AniImg3 active src={img}/></Route3></>};
-        if(4<=batterPosition){return <><Route1><AniImg1 active src={img} /></Route1><Route2><AniImg2 active src={img}/></Route2><Route3><AniImg3 active src={img}/></Route3><Route4><AniImg4 active src={img}/></Route4></>}
-    };
-
-    return (
-        <Wrap>
-            <SBOWrap>
-                <SBO>S <Count style={{color : 'yellow'}}>●●</Count></SBO>
-                <SBO>B<Count style={{color : 'green'}}>●●●</Count></SBO>
-                <SBO>O<Count style={{color : 'red'}}>●●</Count></SBO>
-            </SBOWrap>
-            {runningAnimation()}
-            <PitchBtn onClick={pitchBtnClickHandler}>Pitch!</PitchBtn>
-            <PitchBtnWrap style={btnStyle}><BallBtn onClick={ballBtnClickHandler}>Ball</BallBtn><StrikeBtn onClick={strikeBtnClickHandler}>Strike</StrikeBtn></PitchBtnWrap>
-            <FieldImg src={filedImg} />
-            <GameInfo>2회초 수비</GameInfo>
-        </Wrap>
-    );
+  return (
+    <Wrap>
+      <SBOWrap>
+        <SBO>
+          S <Count style={{ color: "yellow" }}>●●</Count>
+        </SBO>
+        <SBO>
+          B<Count style={{ color: "green" }}>●●●</Count>
+        </SBO>
+        <SBO>
+          O<Count style={{ color: "red" }}>●●</Count>
+        </SBO>
+      </SBOWrap>
+      {runningAnimation()}
+        <PitchBtn click={strikeBtnClickHandler}/>
+      <FieldImg src={filedImg} />
+      <GameInfo>2회초 수비</GameInfo>
+    </Wrap>
+  );
 };
 
 export default Field;
