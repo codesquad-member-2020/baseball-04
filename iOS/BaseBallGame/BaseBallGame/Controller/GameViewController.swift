f0a49ba29b57640121ddd994097c495b43f742af
dev-Lena <keunnalee@gmail.com>
2020-05-16 16:25:35 +0900

    [iOS] feat: 게임 화면 API 연동 - 현재 투수/타자 표시
    - mock API에 Screen Data 요청
    - 현재 투수/타자를 표시하는 View 클래스인 CurrentAtBatView 클래스에 데이터 전달
    - CurrentAtBatView에서 View를 업데이트
    - Issue #31

--- iOS/BaseBallGame/BaseBallGame/Controller/GameViewController.swift
+++ iOS/BaseBallGame/BaseBallGame/Controller/GameViewController.swift
@@ -10,17 +10,63 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var playerImageView: UIImageView!
     @IBOutlet weak var playActionButton: CustomButton!
     @IBOutlet var popView: UIView!
     @IBOutlet weak var historyButton: CustomButton!
-    @IBOutlet weak var missButton: CustomButton!
+    @IBOutlet weak var missButton: UIButton!
     @IBOutlet weak var hitButton: CustomButton!
     var hitButtonCenter: CGPoint!
     var missButtonCenter: CGPoint!
+    @IBOutlet weak var currentBatterPlayer: CurrentAtBatView!
+    @IBOutlet weak var currentPitchPlayer: CurrentAtBatView!
+    @IBOutlet weak var ballLightStackView: UIStackView!
+    @IBOutlet weak var ballCountLabel: UILabel!
+    @IBOutlet weak var inningInfoLabel: UILabel!
+    @IBOutlet weak var strikeCountLabel: UILabel!
+    @IBOutlet weak var strikeLightStackView: UIStackView!
+    @IBOutlet weak var outCountLabel: UILabel!
+    @IBOutlet weak var outLightStackView: UIStackView!
+    @IBOutlet weak var homeTeamLabel: UILabel!
+    @IBOutlet weak var awayTeamLabel: UILabel!
+    @IBOutlet weak var homeRoundView: RectView!
+    
+    @IBOutlet weak var firstBaseView: RectView!
+    
+    @IBOutlet weak var thirdBaseView: RectView!
+    @IBOutlet weak var secondBaseView: RectView!
+    private var baseView = [RectView]()
+    var number = 0
+    private var useCase = UseCase()
+    private var inGameInfo: InGameInfo?
+    private var isOffensive = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.baseView = [homeRoundView ,firstBaseView,thirdBaseView]
         setupView()
+        useCase.getScreenData(){ screenData in
+            self.inGameInfo = screenData
+            self.currentBatterPlayer.isBatter = false
+            self.currentBatterPlayer.configuration = self.inGameInfo
+            self.currentPitchPlayer.isBatter = true
+            self.currentPitchPlayer.configuration = self.inGameInfo
+            
+        }
+    }
+    
+    private func movePlayerAnimation() {
+        UIView.animate(withDuration: 0.3, animations: {
+            DispatchQueue.main.async {
+                self.playerImageView.center = self.baseView[self.number].center
+                self.playerImageView.center = self.missButtonCenter
+                if self.number == 4 {
+                    self.number = 0
+                } else {
+                    self.number += 1
+                }
+            }
+        })
     }
     
     @IBAction func historyButtonTapped(_ sender: Any) {
@@ -33,7 +79,7 @@ class GameViewController: UIViewController {
     }
     
     @IBAction func playActionButtonTapped(_ sender: UIButton) {
-        if sender.currentTitle == PlayButtonCase.Pitch.description {
+        if sender.currentTitle == PlayButtonCase.Hitting.description {
             showHittingOptions(sender: sender)
         } else {
             hideHittingOptions(sender: sender)
@@ -53,7 +99,7 @@ class GameViewController: UIViewController {
             self.hitButton.center = self.playActionButton.center
             self.missButton.center = self.playActionButton.center
         })
-        sender.setTitle(PlayButtonCase.Pitch.description, for: .normal)
+        sender.setTitle(PlayButtonCase.Hitting.description, for: .normal)
     }
     
     private func setupView() {
@@ -63,4 +109,7 @@ class GameViewController: UIViewController {
         missButton.center = playActionButton.center
     }
     
+    @IBAction func hitButtonTapped(_ sender: Any) {
+        self.movePlayerAnimation()
+    }
 }
--- iOS/BaseBallGame/BaseBallGame/View/CurrentAtBatView.swift
+++ iOS/BaseBallGame/BaseBallGame/View/CurrentAtBatView.swift
@@ -11,19 +11,50 @@ import UIKit
 @IBDesignable
 class CurrentAtBatView: UIView {
     
-    @IBInspectable var playerImage: UIImage {
+    var isBatter = true
+    @IBInspectable var playerImage: UIImage? {
         get{
             return image
         }
         set{
-            image = newValue
+            image = newValue!
         }
     }
-    private var image: UIImage = #imageLiteral(resourceName: "PitchingPerson") {
+    var image: UIImage {
         didSet {
             imageView.image = image
         }
     }
+    var configuration: InGameInfo? {
+        didSet {
+            if isBatter {
+                updateHitterView()
+            } else {
+                updatePitcherView()
+            }
+        }
+    }
+    
+    private func updateHitterView() {
+                    DispatchQueue.main.async {
+                        self.roleLabel.text = "타자"
+                        self.nameLabel.text =
+                            self.configuration?.currentAtBat.batterName
+                        guard let hitCount = self.configuration?.currentAtBat.hitCount else {return}
+                        self.infoLabel.text = "\(hitCount)"
+
+        }
+    }
+    
+    private func updatePitcherView() {
+        DispatchQueue.main.async {
+        self.roleLabel.text = "투수"
+        self.nameLabel.text =  self.configuration?.currentAtBat.pitcherName
+            guard let pitchCount = self.configuration?.currentAtBat.pitchCount else {return}
+        self.infoLabel.text = "\(pitchCount)"
+        }
+    }
+    
     var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -77,22 +108,24 @@ class CurrentAtBatView: UIView {
     }()
     
     override init(frame: CGRect) {
+        image = UIImage()
         super.init(frame: frame)
         setupView()
     }
     
     required init?(coder: NSCoder) {
+        image = UIImage()
         super.init(coder: coder)
         setupView()
     }
     
     private func setupView() {
-        imageView.image = image
         infoStackView.addArrangedSubview(spacingLabel)
         infoStackView.addArrangedSubview(roleLabel)
         infoStackView.addArrangedSubview(nameLabel)
         infoStackView.addArrangedSubview(infoLabel)
         totalStackView.addArrangedSubview(imageView)
+        imageView.image = image
         totalStackView.addArrangedSubview(infoStackView)
         self.addSubview(totalStackView)
         setupConstraint()
@@ -104,7 +137,11 @@ class CurrentAtBatView: UIView {
         totalStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         totalStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         totalStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-       imageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5).isActive = true
+        setupImageView()
+    }
+    
+    private func setupImageView() {
+        imageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5).isActive = true
         imageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
     }
 }
